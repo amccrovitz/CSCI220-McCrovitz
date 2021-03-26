fe8470badb6b36c425b8f7029b9f0e499036599c
Anaise McCrovitz <amccrovitz@gmail.com>
2021-03-26 17:41:51 -0400

    got linethrough to function

--- Week11/todo.js
+++ Week11/todo.js
@@ -1,12 +1,24 @@
-function AddItem() {
+//add list item from input text
+function addItem() {
+
     var list = document.getElementsByTagName('ul')[0];
     var newLI = document.createElement('li');
     var inputTodo = document.getElementById("todoItem").value;
     var newTodo = document.createTextNode(inputTodo);
 
-
+    //add to the bottom of the list
     newLI.appendChild(newTodo);
     list.appendChild(newLI);
 
 
+
+}
+//Clearing the list
+function clearList() {
+    var row = document.getElementsByTagName("ul");
+    row[0].innerHTML = "";
+}
+//sets the attribute class to checked for css
+function done() {
+    document.getElementsByTagName("LI")[0].setAttribute("class", "checked");
 }
